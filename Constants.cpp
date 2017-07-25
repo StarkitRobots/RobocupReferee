a82986d189ad24d7a850763c4f2a438640f1b6ed
medrimonia <medrimonia@gmail.com>
2017-07-25 06:45:15 +0200

    Updating field size in constants

--- Constants.cpp
+++ Constants.cpp
@@ -20,16 +20,28 @@ namespace RhobanReferee{
     const char * Constants::game_state_header = "RGme";
 
     // FIELD SIZES at EirLab
+    // const double Constants::lineWidth         =   5;
+    // const double Constants::centerRadius      =  75;
+    // const double Constants::borderStripWidth  =  28;
+    // const double Constants::penaltyMarkDist   = 210;
+    // const double Constants::penaltyMarkLength =  10;
+    // const double Constants::goalWidth         = 260;
+    // const double Constants::goalDepth         =  50;
+    // const double Constants::goalAreaLength    = 100;
+    // const double Constants::goalAreaWidth     = 500;
+    // const double Constants::fieldLength       = 810;
+    // const double Constants::fieldWidth        = 600;
+    // FIELD SIZES at Nagoya
     const double Constants::lineWidth         =   5;
     const double Constants::centerRadius      =  75;
-    const double Constants::borderStripWidth  =  28;
+    const double Constants::borderStripWidth  =  70;//Minimum value
     const double Constants::penaltyMarkDist   = 210;
     const double Constants::penaltyMarkLength =  10;
     const double Constants::goalWidth         = 260;
-    const double Constants::goalDepth         =  50;
+    const double Constants::goalDepth         =  60;
     const double Constants::goalAreaLength    = 100;
     const double Constants::goalAreaWidth     = 500;
-    const double Constants::fieldLength       = 810;
+    const double Constants::fieldLength       = 900;
     const double Constants::fieldWidth        = 600;
     // EXPECTED FIELD SIZES
     // const double Constants::lineWidth         =   5;
