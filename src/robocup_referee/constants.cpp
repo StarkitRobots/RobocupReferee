9c3fad8133bdb3c53db5444fef814c086cfa2854
Adrien Boussicault <boussica@labri.fr>
2018-04-03 19:33:26 +0200

    Add constants for the field.

--- src/robocup_referee/constants.cpp
+++ src/robocup_referee/constants.cpp
@@ -32,17 +32,31 @@ namespace robocup_referee{
     // const double Constants::fieldLength       = 810;
     // const double Constants::fieldWidth        = 600;
     // FIELD SIZES at Nagoya
-    const double Constants::lineWidth         =   5;
-    const double Constants::centerRadius      =  75;
-    const double Constants::borderStripWidth  =  70;//Minimum value
-    const double Constants::penaltyMarkDist   = 210;
-    const double Constants::penaltyMarkLength =  10;
-    const double Constants::goalWidth         = 260;
-    const double Constants::goalDepth         =  60;
+    // const double Constants::lineWidth         =   5;
+    // const double Constants::centerRadius      =  75;
+    // const double Constants::borderStripWidth  =  70;//Minimum value
+    // const double Constants::penaltyMarkDist   = 210;
+    // const double Constants::penaltyMarkLength =  10;
+    // const double Constants::goalWidth         = 260;
+    // const double Constants::goalDepth         =  60;
+    // const double Constants::goalAreaLength    = 100;
+    // const double Constants::goalAreaWidth     = 500;
+    // const double Constants::fieldLength       = 900;
+    // const double Constants::fieldWidth        = 600;
+
+    // FIELD SIZES at Nagoya
+    const double Constants::lineWidth         =   6;
+    const double Constants::centerRadius      =  78;
+    const double Constants::borderStripWidth  =  65;//Minimum value
+    const double Constants::penaltyMarkDist   = 209;
+    const double Constants::penaltyMarkLength =  15;
+    const double Constants::goalWidth         = 256;
+    const double Constants::goalDepth         =  61;
     const double Constants::goalAreaLength    = 100;
-    const double Constants::goalAreaWidth     = 500;
+    const double Constants::goalAreaWidth     = 487;
     const double Constants::fieldLength       = 900;
     const double Constants::fieldWidth        = 600;
+
     // EXPECTED FIELD SIZES
     // const double Constants::lineWidth         =   5;
     // const double Constants::centerRadius      =  75;
